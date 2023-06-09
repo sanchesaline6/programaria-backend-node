2ed1511fcb7cf832c1fde93e2f057d92fa455172
sanchesaline6 <sanches.aline6@gmail.com>
2023-06-09 08:52:20 -0300

    implementa métodos para inserção, atualização e remoção de mulher

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const porta = 3333
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const app = express()
 const router = express.Router()
 
@@ -9,28 +10,38 @@ const mulher = {
     minibio: 'Analista de Sistemas migrando para área de dados'
 }
 
-const mulheres = [
+let mulheres = [
     {
+        id: '1',
         nome: 'Simara Conceição',
         imagem: 'https://github.com/simaraconceicao.png',
         minibio: 'Desenvolvedora e Instrutora'
     },
     {
+        id: '2',
         nome: 'Iana Chan',
         imagem: 'https://bit.ly/3JCXBqP',
         minibio: 'Fundadora da Programaria'
     },
     {
+        id: '3',
         nome: 'Nina da Hora',
         imagem: 'https://bit.ly/3FKpFaz',
         minibio: 'Hacker Antirracista'
     }
 ]
-app.use(router.get('/ola', mostraOlaMundo))
+
+app.use(express.json())
+app.use(router.get('/', mostraOlaMundo))
 app.use(router.get('/mulher', mostraInfoMulher))
 app.use(router.get('/mulheres', mostraInfoMulheres))
 app.use(router.get('/hora', mostraHora))
 
+app.use(router.post('/mulheres', criaMulher))
+
+app.use(router.patch('/mulheres/:id', atualizaMulher))
+
+app.use(router.delete('/mulheres/:id', removeMulher))
 app.listen(porta, mostraPorta)
 
 function mostraOlaMundo(request, response) {
@@ -54,4 +65,46 @@ function mostraHora(request, response){
     const hora = data.toLocaleTimeString();
 
     response.send(hora);
+}
+
+function criaMulher(request, response) {
+    const novaMulher = {
+        id: uuidv4(),
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio
+    }
+
+    mulheres.push(novaMulher)
+    response.json(mulheres)
+}
+
+function atualizaMulher(request, response){
+    
+    const mulherEncontrada = encontraMulher(request.params.id)
+
+    if(request.body.nome) {
+        mulherEncontrada.nome = request.body.nome
+    }
+    if(request.body.minibio) {
+        mulherEncontrada.minibio = request.body.minibio
+    }
+    if(request.body.imagem) {
+        mulherEncontrada.imagem = request.body.imagem
+    }
+
+    response.json(mulheres)
+}
+
+function removeMulher(request, response) {
+    const mulherEncontrada = encontraMulher(request.params.id)
+    mulheres = mulheres.filter(mulher => mulher.id !== mulherEncontrada.id)
+    response.json(mulheres)
+
+}
+
+function encontraMulher(id) {
+    
+    return mulheres.find(mulher => mulher.id === id)
+
 }
